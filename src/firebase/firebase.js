94dfbb3d5129982c4448c31ad57022f915032dc8
Radek <radek@fanaticdesign.co.uk>
2020-06-17 23:16:28 +0100

    149. Array Data in Firebase: Part I

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,25 +14,107 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
+
+database.ref('expenses').push({
+  description: 'Course Topic!',
+  note: 'React, Python',
+  amount: 2450,
+  createdAt: 123987
+});
+
+// database.ref('expenses').push({
+//   description: 'Game Topic!',
+//   note: 'React, Python',
+//   amount: 350,
+//   createdAt: 13987
+// });
+
+// database.ref('expenses').push({
+//   description: 'React Topic!',
+//   note: 'React, Python',
+//   amount: 22450,
+//   createdAt: 1244987
+// });
+
+
+// const notes = [{
+//   id: '12',
+//   title: 'First note!',
+//   body: 'This is my note'
+// }, {
+//   id: '342',
+//   title: 'Second note!',
+//   body: 'This is my note'
+// }]
+
+// database.ref('notes').set(notes);
+
+// database.ref()
+//   .once()
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.log('SOmething went wrong', err)
+//   })
+
+// database.ref()
+//   .on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is ${val.job.title} at ${val.job.company}`)
+//   })
+
+//   setTimeout(() => {
+//     database.ref().update({
+//       name: 'Radek Jedrzej',
+//       'job/title': 'Sprzatacz'
+//     })
+//     .then(() => {
+//       console.log('all good')
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }, 3000)
+
+
 // database.ref().set({
 //   name: 'Radek Jedrej',
 //   age: 32,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
 //   isSingle: true,
 //   location: {
 //     city: 'Kwidzyn',
 //     country: "Poland"
 //   }
 // }).then(() => {
+//   console.log('Data added')
+// }).catch(err => {
+//   console.log('Something went wrong')
+// })
+
+// database.ref().update({
+//   name: 'Edyta Zelma',
+//   age: 27,
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   isSingle: null,
+//   'location/city': 'Rynek'
+// }).then(() => {
 //   console.log('Database updated')
 // }).catch(err => {
 //   console.log('Something went wrong')
 // })
 
-database.ref('isSingle')
-  .remove()
-  .then(() => {
-    console.log('Data Was Removed')
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// database.ref('isSingle')
+//   .remove()
+//   .then(() => {
+//     console.log('Data Was Removed')
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
